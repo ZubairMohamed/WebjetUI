c5b5f01167184107880796777f3fe4fdc786b78d
zmoh <zubairnz@outlook.com>
2024-12-24 19:18:09 +1300

    tailwind was added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.less'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-          hello world
-      </div>
+          <div className="container mx-auto bg-amber-300">
+              hello world
+          </div>
     </>
   )
 }
