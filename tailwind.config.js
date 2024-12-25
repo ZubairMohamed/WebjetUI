96e63f2b8afb3dc283c7bc90977b9ed6ba07d569
zmoh <zubairnz@outlook.com>
2024-12-26 12:45:52 +1300

    decide on design breakpoints using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'tablet': '768px',
+      // Tablet => @media (min-width: 768px) { ... }
+
+      'laptop': '992px',
+      // Tablet or laptop => @media (min-width: 992px) { ... }
+
+      'desktop': '1200px',
+      // Desktop or larger => @media (min-width: 1200px) { ... }
+    },
     extend: {},
   },
   plugins: [],
