6b8ab9398965b7000c5cadc64b03decec3d01667
zmoh <zubairnz@outlook.com>
2024-12-26 08:57:32 +1300

    work out the Header breakpoints and updated the logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
+// Dimensions (Width x Height)
+// Desktop 1384px x 98px
+// Tablet 768px x 98px
+// Mobile 376px x 58px
+
 const Header = () => {
     return (
         <>
-            <div className="container mx-auto">
-                Hello from header
+            <div className="container brand-bg-1 mx-auto w-[1384px] h-[98px] flex justify-center sm:justify-start ">
+                {/*Hello from header*/}
+                <img src="https://media.webjet.com.au/ui/images/webjet-logo-au-red.png" alt="webjet-logo" />
             </div>
         </>
     )
