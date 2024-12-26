1980f206b4d05bd3d0216becfdb615ce2e4fc90e
zmoh <zubairnz@outlook.com>
2024-12-26 14:33:47 +1300

    cleaned up Header to only have a container on desktop and above

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@
 const Header = () => {
     return (
         <>
-            <div className="container brand-bg-1 mx-auto w-[1384px] h-[98px] flex items-center justify-center tablet:justify-start">
+            <div className="desktop:container brand-bg-1 mx-auto w-full desktop:w-[1384px] h-[58px] tablet:h-[98px] flex items-center justify-center tablet:justify-start">
                 {/*Hello from header*/}
 
                     <img src="https://media.webjet.com.au/ui/images/webjet-logo-au-red.png" alt="webjet-logo"
-                         className='h-[73px] pl-[12px]' />
+                         className='h-[42px] tablet:h-[73px] tablet:pl-[12px]' />
 
             </div>
         </>
