0c949f65d5df3cc365be38fe3d25e8966598c8a4
zmoh <zubairnz@outlook.com>
2024-12-27 13:28:23 +1300

    restructured the search interface to have a separate title from the other children

--- src/components/SearchInterface/SearchInterface.jsx
+++ src/components/SearchInterface/SearchInterface.jsx
@@ -1,6 +1,6 @@
 import * as PropTypes from "prop-types";
 
-SearchInterface.propTypes = {children: PropTypes.any}
+SearchInterface.propTypes = {children: PropTypes.any, title: PropTypes.string};
 
 /**
  * This component is the parent component to the entire search interface consisting of filters, search results, banner, etc.
@@ -8,11 +8,14 @@ SearchInterface.propTypes = {children: PropTypes.any}
  * @returns {JSX.Element}
  * @constructor
  */
-export default function SearchInterface({children}){
+export default function SearchInterface({children, title}){
     return (
         <>
-            <div className="w-full desktop:container desktop:mx-auto bg-white pl-[12px] pr-[12px]">
-                {children}
+            <div className="w-full desktop:container desktop:mx-auto bg-white pl-[12px] pr-[12px] pb-[20px] ">
+                {title}
+                <div className="flex flex-col desktop:flex-row gap-5">
+                    {children}
+                </div>
             </div>
         </>
     )
