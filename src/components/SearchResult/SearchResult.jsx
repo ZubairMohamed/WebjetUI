cad51236ef1f8b8cb0582137ddc65f49c529b1ba
zmoh <zubairnz@outlook.com>
2024-12-29 20:06:03 +1300

    the ui of a hotel has been updated

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -23,11 +23,19 @@ export default function SearchResult({title, stars, roomType, price, isLast = fa
         <>
             {/* Controls whether to display the result in searches */}
             {displayResult ? (
-                <div className={`w-full h-[180px] border border-solid border-gray-400 rounded-md  ${isLast == false && 'mb-5'} shadow-md`}>
-                    <p>Title is: {title}</p>
-                    <p>Stars is: {stars}</p>
-                    <p>Room Type is: {roomType}</p>
-                    <p>Price is: {price}</p>
+                <div className={`flex w-full h-[180px] border border-solid border-gray-400 rounded-md  ${isLast == false && 'mb-5'} shadow-md`}>
+                    <div className="image grow shrink laptop:basis-[186px] desktop:basis-[186px]">
+                        this is an image
+                    </div>
+                    <div className="desc grow shrink laptop:basis-[374px] desktop:basis-[374px] ">
+                        <p className={'roboto-bold text-base'}>{title}</p>
+                        <p>{stars} stars</p>
+                        <p className={'text-[13px] whitespace-nowrap '}><span className={"roboto-bold"}>Room Type:</span> {roomType}</p>
+                    </div>
+                    <div className="price grow-0 shrink basis-[170px] text-[26px] roboto-bold bg-grey-1 flex justify-end p-[26px] ">
+                        ${price}
+                    </div>
+
                 </div>
                 ) : null}
 
