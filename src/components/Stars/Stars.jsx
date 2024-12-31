808439dee985537c5055dd9b8df0ea777c6d5205
zmoh <zubairnz@outlook.com>
2024-12-31 18:11:18 +1300

    stars component now support .5 display

--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -1,6 +1,7 @@
 import * as PropTypes from 'prop-types';
 import { GiDiamonds } from 'react-icons/gi';
 import classes from './Stars.module.less';
+import { Fragment } from 'react';
 
 Stars.propTypes = {
     numberOfStars: PropTypes.number,
@@ -17,11 +18,21 @@ export default function Stars({ numberOfStars, isFilter = true }) {
         <div className="flex items-center">
             {Array.from({ length: numberOfStars }).map((_, index) => (
                 // <ImDiamonds key={index} className={'icon-fill-1'}  />
-                <GiDiamonds
-                    key={index}
-                    className={classes.iconFill}
-                    size={20}
-                />
+                <Fragment key={index}>
+                    <GiDiamonds className={classes.iconFill} size={20} />
+                    {/*
+                        - here we are first checking if fraction is true
+                        - next we are checking if currently we are on the last loop
+                        - the last loop is equal to the number of elements in our array - 1
+                        - lastly we are subtracting 0.5 as our number contains a fractional value
+                     */}
+                    {fraction && index === numberOfStars - 1 - 0.5 && (
+                        <GiDiamonds
+                            className={`${classes.half} ${classes.iconFill}`}
+                            size={20}
+                        />
+                    )}
+                </Fragment>
             ))}
             {isFilter && <p className="text-sm ml-0.5">&amp; up</p>}
         </div>
